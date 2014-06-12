e4379b46ae02a5f9d120e6afc6adb2a1ad8f8abb
Matt Willsher <matt@willsherpartners.co.uk>
2014-06-12 10:16:43 +0100

    Add data checkout, improve loading script

--- roles/IHTSDO.otf-mapping-service-admin/files/load_data.sh
+++ roles/IHTSDO.otf-mapping-service-admin/files/load_data.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -x
+set -e
+
 if [[ $( whoami ) != 'otf-mapping-service-rest' ]]; then
-	echo "This script must be run as user otf-mapping-service-rest"
-    exit 1
+    exec sudo -u otf-mapping-service-rest -H $0
 fi
 
 export OTF_MAPPING_HOME=/opt/otf-mapping-service-admin
 export OTF_MAPPING_CONFIG=/etc/opt/otf-mapping-service-rest/config.properties
 csh /opt/otf-mapping-service-admin/admin/loader/src/main/scripts/load.csh
+
 
